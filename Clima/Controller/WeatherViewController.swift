2e4daa845641d7922f79c3123cab20409ea5b697
gred-wasntavailable <griffin_reding@hotmail.com>
2022-07-27 18:31:07 -0700

    Initial Project Setup
    App in it's state after the first lesson or so of the unit.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            searchTextField.text = ""
+            return true
+        } else {
+            textField.placeholder = "Hey dumbass, city and state."
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) -> String {
+        let desiredLoc = searchTextField.text
+        searchTextField.text = ""
+        return desiredLoc ?? ""
+    }
+    
 }
 
